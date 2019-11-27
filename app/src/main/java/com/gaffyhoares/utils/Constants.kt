86bbefea55cbb2b0879b8c3abf6828ff487f29a6
ARSOFT1 <ar.soft1245@gmail.com>
2019-11-27 15:10:02 +0500

    Movie categories are added

--- app/src/main/java/com/gaffyhoares/utils/Constants.kt
+++ app/src/main/java/com/gaffyhoares/utils/Constants.kt
@@ -3,7 +3,6 @@ package net.gahfy.feedme.utils
 /** The base URL of the API */
 const val BASE_URL = "https://api.themoviedb.org/3/"
 const val API_KEY = "d85176d3c754f545365caa4a5a9d84f5"
-const val MOVE_CATEGORY = "popular"
 const val IMG_BASE_URL = "http://image.tmdb.org/t/p/w185/"
 
 const val URL = "http://nigerianstudentshop.com"
